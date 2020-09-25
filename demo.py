53699e382082013fe337dd30855d31a343271c31
Brian Faivre <brian.faivre@gmail.com>
2020-09-24 22:23:23 -0700

    Add 100 gravity readings per FV.

--- demo.py
+++ demo.py
@@ -1,6 +1,9 @@
+import random
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Fv
+from models import Fv, Gravity
 
 engine = create_engine("mysql://root:root@localhost/Demo")
 connection = engine.connect()
@@ -9,9 +12,17 @@ connection.execute(f"CREATE DATABASE IF NOT EXISTS Demo")
 Session = sessionmaker(bind = engine)
 session = Session()
 
-for n in range(1, 10):
-    fv = Fv(Name = f"FV0{n}")
-    print(f"Adding FV0{n}.")
-    session.add(fv)
+# for n in range(1, 10):
+#     fv = Fv(Name = f"FV0{n}")
+#     print(f"Adding FV0{n}.")
+#     session.add(fv)
+
+# session.commit()
+
+for fv in session.query(Fv).all():
+    for n in range (0, 101):
+        gravityReading = random.randint(10, 17)
+        gravity = Gravity(FvId = fv.FvId, Gravity = gravityReading, Timestamp = datetime.now() - relativedelta(days = n))
+        session.add(gravity)
 
 session.commit()
