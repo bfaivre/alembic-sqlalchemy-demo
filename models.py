ac1a1304696bea4ce90f4fe7a1f272976153a466
Brian Faivre <brian.faivre@gmail.com>
2020-09-24 22:07:24 -0700

    Added Gravity table.

--- models.py
+++ models.py
@@ -15,3 +15,12 @@ class Fv(Base):
     FvId = Column(Integer, primary_key = True)
     Name = Column(String(45), nullable = False)
     Volume = Column(Integer, nullable = True)
+
+    Gravities = relationship("Gravity", backref = "FV", lazy = "dynamic")
+
+class Gravity(Base):
+    __tablename__ = "Gravity"
+    GravityId = Column(Integer, primary_key = True)
+    FvId = Column(Integer, ForeignKey("Fv.FvId"), nullable = False) 
+    Gravity = Column(Float, nullable = False)
+    Timestamp = Column(DATETIME(fsp = 6), nullable = False)
